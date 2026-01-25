6a65dabd9757621e40afb47a2b4aa5a96877d596
SeinalArifin149 <arifinseinals3@gmail.com>
2026-01-25 20:41:28 +0700

    Add link stylesheet in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import 'devicon/devicon.min.css'
+// import 'devicon/devicon.min.css'
 import { Home, About, Contact, Portofolio } from './pages'
 import { Main } from './components'
 

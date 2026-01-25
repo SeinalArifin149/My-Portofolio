9a225d38308dabe8c3b00ced7f9c15d0984ed57e
SeinalArifin149 <arifinseinals3@gmail.com>
2026-01-25 18:47:00 +0700

    change 3

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-// import './navbar.css'
-import { Link } from 'react-router-dom'
 export function Navbar() {
   return (
     <nav className="flex items-center justify-between px-6 py-4 fixed top-0 left-0 w-full z-50 bg-black/80 backdrop-blur-md">

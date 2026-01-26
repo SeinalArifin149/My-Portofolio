43e2bc6718df9fa055127ab97ff67f18b02e8085
SeinalArifin149 <arifinseinals3@gmail.com>
2026-01-26 07:42:56 +0700

    ChaChange vite minify to esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 })

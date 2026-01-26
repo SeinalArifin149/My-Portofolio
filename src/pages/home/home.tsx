22586b4f70785656f367885ba0a1e9abdd352e06
SeinalArifin149 <arifinseinals3@gmail.com>
2026-01-26 14:41:05 +0700

    Modifying Css Home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,7 @@ import { Knowledge } from './knowledge';
 
 export function Home() {
   return (
-    <main className="container mx-auto px-6 py-10 lg:py-0 lg:h-[calc(100vh-100px)] flex items-center relative z-10 mt-20">
+    <main className="container mx-auto  py-10 lg:py-0 lg:h-[calc(100vh-100px)] flex items-center relative z-10">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 w-full items-center">
         <Text />
         <Profile />

81d62d20bfdb920403c40558f78f7031d98b1d51
SeinalArifin149 <arifinseinals3@gmail.com>
2026-01-30 00:59:22 +0700

    NPm run Build gain

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 // import { Activity } from 'react'
 // import { Actifity } from './activity/activity'
-
+// aman
 export { Home } from './home/home'
 export { Contact} from './contact/contact'
 export { Activity } from './activity/activity'

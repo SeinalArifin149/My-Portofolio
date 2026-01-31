68ef4ce81a8806c0448736e093b61c15d16be661
SeinalArifin149 <arifinseinals3@gmail.com>
2026-01-31 13:06:41 +0700

    add transition page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,49 @@
-// import React from 'react';
-import { Profile } from './profile';
-import { Text } from './text';
-import { Knowledge } from './knowledge';
+import { motion } from "framer-motion";
+import { Profile } from "./profile";
+import { Text } from "./text";
+import { Knowledge } from "./knowledge";
 
 export function Home() {
   return (
-    <main className="container mx-auto  py-10 lg:py-0 lg:h-[calc(100vh-100px)] flex items-center relative z-10">
+    <motion.main
+      className="container mx-auto py-10 lg:py-0 lg:h-[calc(100vh-100px)] flex items-center relative z-10"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+    >
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 w-full items-center">
-        <Text />
-        <Profile />
-        <Knowledge />
+
+        {/* Text */}
+        <motion.div
+          className="lg:col-span-5"
+          initial={{ x: -50, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+        >
+          <Text />
+        </motion.div>
+
+        {/* Profile */}
+        <motion.div
+          className="lg:col-span-4"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+        >
+          <Profile />
+        </motion.div>
+
+        {/* Knowledge */}
+        <motion.div
+          className="lg:col-span-3"
+          initial={{ y: 40, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.4 }}
+        >
+          <Knowledge />
+        </motion.div>
+
       </div>
-    </main>
+    </motion.main>
   );
 }

60a37227da448015bf1b7ed6aec8b5d2066372a5
SeinalArifin149 <arifinseinals3@gmail.com>
2026-03-05 19:13:51 +0700

    editor Profile in contact

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,7 +1,11 @@
 // import { Navbar, Main } from '../../components'
+import { Profile_Contact } from "./profile"
+import { Text_Contact } from "./text"
 export function Contact() {
     return(
         <div className="">
+            <Profile_Contact/>
+            <Text_Contact/>
 
         {/* <Navbar /> */}
       {/* <h1 className="text-white text-3xl">Ini halaman Hkontak</h1>
--- src/pages/contact/text.tsx
+++ src/pages/contact/text.tsx
@@ -1,4 +1,4 @@
-export function text() {
+export function Text_Contact() {
     <p>
         Want to make awesome and impactful Product?
     </p>
